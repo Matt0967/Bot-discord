3074ff379a16248acec7da2a8badc65e2f973cd5
Matthieu <[perezmatthieu1@gmail.com]>
2024-10-20 13:49:01 +0200

    vérification du code avec github

--- commandes-slasch.py
+++ commandes-slasch.py
@@ -3,12 +3,12 @@ from discord.ext import commands
 
 token = "MTI4NzA4ODYxODM2MzY4Njk3Mg.GAAk2I.DPnDCNRPJWosVv5EaPpBSUfDqt-u6POnYTVD4Y"
 intents = discord.Intents.all()
-bot = commmands.bot(command_prefix="!", intents=intents )
+bot = commands.Bot(command_prefix="!", intents=intents )
 
 
 
 def main():
-    bot.run = (token=)
+    bot.run(token)
 
-if __name__ == '__main__'
+if __name__ == '__main__':
     main()
