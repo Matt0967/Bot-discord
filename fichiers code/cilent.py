69bda974f1b2f7f0285db0aee9f216272ed62a0b
Matthieu <[perezmatthieu1@gmail.com]>
2024-10-21 14:13:07 +0200

    changement-rapide-et-commande/

--- fichiers code/cilent.py
+++ fichiers code/cilent.py
@@ -1,6 +1,6 @@
 import discord 
 
-token = "MTI4NzA4ODYxODM2MzY4Njk3Mg.GAAk2I.DPnDCNRPJWosVv5EaPpBSUfDqt-u6POnYTVD4Y"
+token = "MTI4NzA4ODYxODM2MzY4Njk3Mg.GJHyfW.7oGzMiBeJG5vHChVAPEksB2Nx3JvACqAnrwp9M"
 
 client = discord.Client(intents=discord.Intents.all())
 
