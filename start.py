b774542004c9087a3315e022083c0980074cd652
Matthieu <[perezmatthieu1@gmail.com]>
2024-11-07 16:21:12 +0100

    ajouté-extenstion

--- start.py
+++ start.py
@@ -10,5 +10,18 @@ token = os.getenv('DISCORD_TOKEN')
 intents = discord.Intents.all()
 bot = commands.Bot(command_prefix='!', intents=intents)
 
+# Charger les extensions
+@bot.event
+async def on_ready():
+    for extension in ['commande-slash']:
+        try:
+            await bot.load_extension(f'cogs.{extension}')
+        except Exception as e:
+            print(f'Une erreur est survenue lors du chargement de l\'extension {extension}: {type(e).__name__} - {e}')
+
+@bot.event
+async def on_ready():
+    print(f'{bot.user.name} a rejoint le serveur !')
+
 keep_alive()
 bot.run(token=token)
