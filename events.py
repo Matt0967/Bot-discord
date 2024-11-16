a3af8c8c317b89d7da5eea05ed0c374c5ffea541
Mattou_DEV <144120980+Matt0967@users.noreply.github.com>
2024-11-16 12:21:55 +0100

    Add experience points system
    Fixes #1
    
    V2 test
    
    ---
    
    For more details, open the [Copilot Workspace session](https://copilot-workspace.githubnext.com/Matt0967/Bot-discord/issues/1?shareId=XXXX-XXXX-XXXX-XXXX).

--- events.py
+++ events.py
@@ -7,13 +7,19 @@ token=os.getenv('DISCORD_TOKEN')
 
 client = discord.Client(intents=discord.Intents.all())
 
+# Dictionary to store user experience points
+user_exp = {}
+
 @client.event
 async def on_message(message: discord.Message):
    if message.author == client.user:
        return
    elif message.content.lower().startswith("hello"):
         await message.channel.send("bonjour, c'est le bot de test")
-
+   else:
+        if message.author.id not in user_exp:
+            user_exp[message.author.id] = 0
+        user_exp[message.author.id] += 1
 
 @client.event
 async def on_message_delete(message: discord.Message):
