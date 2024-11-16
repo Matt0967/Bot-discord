c9219a006cfd7732cab974194d84222445c5b65b
Matthieu <[perezmatthieu1@gmail.com]>
2024-11-16 12:44:56 +0100

    exp-project

--- commande-slash.py
+++ commande-slash.py
@@ -7,8 +7,8 @@ from discord import ButtonStyle, ui
 import datetime
 import os 
 from dotenv import load_dotenv
-load_dotenv()
-token=os.getenv('DISCORD_TOKEN')
+
+token ='MTI4NzA4ODYxODM2MzY4Njk3Mg.GJHyfW.7oGzMiBeJG5vHChVAPEksB2Nx3JvACqAnrwp9M'
 
 intents = discord.Intents.default()
 bot = commands.Bot(command_prefix="!", intents=intents)
@@ -18,7 +18,7 @@ bot = commands.Bot(command_prefix="!", intents=intents)
 async def pomodoro(interaction: discord.Interaction):
     class PomodoroView(ui.View):
         def __init__(self):
-            super().__init__(timeout=5)  # 5 secondes pour choisir
+            super().__init__(timeout=2)  # 2 secondes pour choisir
             self.choice = None
 
         @ui.button(label="25-5", style=ButtonStyle.primary, emoji="⏱️")
@@ -137,18 +137,18 @@ async def citation(interaction: discord.Interaction):
         "La vie est un mystère qu'il faut vivre, et non un problème à résoudre. - Gandhi",
         "Le plus grand risque est de ne prendre aucun risque. - Mark Zuckerberg",
         "Le succès, c'est se promener d'échecs en échecs tout en restant motivé. - Winston Churchill",
-        "Votre temps est limité, ne le gâchez pas en vivant la vie de quelqu'un d'autre. - Steve Jobs"
-        "Le succès n'est pas la clé du bonheur. Le bonheur est la clé du succès. Si vous aimez ce que vous faites, vous réussirez. – Albert Schweitzer"
-        "Le seul endroit où le succès vient avant le travail est dans le dictionnaire – Vidal Sassoon"
-        "Ne rêve pas ta vie, vis ton rêve – Anonyme"
-        "Les grandes choses ne sont jamais faites par une seule personne, elles sont faites par une équipe – Steve Jobs"
-        "Le plus grand échec est de ne pas avoir le courage d'oser – Abbé Pierre"
-        "Il ne s’agit pas d’être le meilleur, mais d’être meilleur que la personne que nous étions hier – Anonyme"
-        "La seule limite à notre épanouissement de demain sera nos doutes d’aujourd’hui – Franklin D. Roosevelt"
-        "Le succès consiste à aller d'échec en échec sans perdre son enthousiasme – Winston Churchill"
-        "Vous ne pouvez pas changer votre passé, mais vous pouvez ruiner le présent en vous inquiétant de l'avenir – Anonyme"
-        "Les difficultés préparent les gens ordinaires à un destin extraordinaire – C.S. Lewis"
-        "Le courage n'est pas l'absence de peur, mais la capacité de vaincre ce qui fait peur – Nelson Mandela"
+        "Votre temps est limité, ne le gâchez pas en vivant la vie de quelqu'un d'autre. - Steve Jobs",
+        "Le succès n'est pas la clé du bonheur. Le bonheur est la clé du succès. Si vous aimez ce que vous faites, vous réussirez. – Albert Schweitzer",
+        "Le seul endroit où le succès vient avant le travail est dans le dictionnaire – Vidal Sassoon",
+        "Ne rêve pas ta vie, vis ton rêve – Anonyme",
+        "Les grandes choses ne sont jamais faites par une seule personne, elles sont faites par une équipe – Steve Jobs",
+        "Le plus grand échec est de ne pas avoir le courage d'oser – Abbé Pierre",
+        "Il ne s’agit pas d’être le meilleur, mais d’être meilleur que la personne que nous étions hier – Anonyme",
+        "La seule limite à notre épanouissement de demain sera nos doutes d’aujourd’hui – Franklin D. Roosevelt",
+        "Le succès consiste à aller d'échec en échec sans perdre son enthousiasme – Winston Churchill",
+        "Vous ne pouvez pas changer votre passé, mais vous pouvez ruiner le présent en vous inquiétant de l'avenir – Anonyme",
+        "Les difficultés préparent les gens ordinaires à un destin extraordinaire – C.S. Lewis",
+        "Le courage n'est pas l'absence de peur, mais la capacité de vaincre ce qui fait peur – Nelson Mandela",
     ]
     quote = random.choice(quotes)
     await interaction.response.send_message(quote)
